b66c1748a09ed91266c5ee6265e17a69b8ba4b3f
Khaleed Saifullah Nayem <ksnayem@gmail.com>
2024-05-19 00:14:23 +0600

    project up

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  new WOW().init();
+  $("body").niceScroll({
+    cursorcolor: "#28377e",
+    cursorwidth: "5px",
+    cursoropacitymax: 1,
+    boxzoom: true,
+    cursorborder: "1px solid #28377e",
+  });
+});
